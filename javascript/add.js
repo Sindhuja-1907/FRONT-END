9ac242f9afad6d9c262d64230558671ac2957ad1
Sindhuja-1907 <sindhuja.mylavarapu1@gmail.com>
2025-03-12 10:08:00 -0400

    day_2 code

---
+++ javascript/add.js
@@ -0,0 +1,7 @@
+<script>
+    console.time(`start`);
+    let a = Number(window.prompt("Enter 1st number:"));
+    let b = Number(window.prompt("Enter 2nd number:"));
+    document.write(`Sum of 2 numbers: ${a + b}`);
+    console.timeEnd(`end`);
+</script>
